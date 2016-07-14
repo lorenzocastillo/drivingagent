92175cf68f3d8a962b66e7118504fa1218164d7d
lorenzocastillo <lcast042@fiu.edu>
2016-07-14 08:49:51 -0400

    Refactored Code

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -1,120 +1,127 @@
 import random
+import sys
+import pandas as pd
+import numpy as np
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 from itertools import permutations,combinations
-import sys
-import pandas as pd
-import numpy as np
 
 
-MIN_INT = -sys.maxint - 1
+MIN_INT = -(sys.maxint - 1)
+SHOULD_FORWARD = 0
+SHOULD_LEFT = 1
+SHOULD_RIGHT = 2
+SHOULD_YIELD = 3
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
-    def __init__(self, env, gamma = 0.8, alpha = 0.1, epsilon = 100):
-        super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
+    def __init__(self, env, gamma=0.8, alpha=0.1, epsilon=100):
+        super(LearningAgent, self).__init__(env)
         self.color = 'red'  # override color
-        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initialize any additional variables here
-        self.state = (None, None, None, None)
-        self.Q = {self.state : [0,0,0,0]}
+        self.planner = RoutePlanner(self.env, self)
+
+        self.state = None
+        self.Q = {self.state: [0, 0, 0, 0]}
         self.decrement_epsilon = True
         self.epsilon = epsilon
         self.gamma = gamma
         self.alpha = alpha
         
         #STATS VARIABLES
-        self.deadlines = []
-        self.rewards = []
+        self.deadlines = list()
+        self.rewards = list()
         self.reward_for_run = 0
         self.deadline_for_run = 0
-    
-#    def state_from_input(self,input,waypoint):
-#        return (input['light'],input['left'],input['oncoming'],waypoint)
-    #Eliminate redundant states
-    def state_from_input(self,input,waypoint):
+
+    def state_from_input(self, input, waypoint):
+        """
+        Reduces the Action Space based on states
+        :param input:
+        :param waypoint:
+        :return:
+        """
         if input['light'] == 'green':
             if waypoint == 'forward':
-                return "Should Go Forward"
+                return SHOULD_FORWARD
             elif waypoint == 'left':
-                if input['oncoming'] == None:
-                    return "Should Go Left"
+                if input['oncoming'] is None:
+                    return SHOULD_LEFT
                 else:
-                    return "Should Yield"
+                    return SHOULD_YIELD
             else:
-                return "Should Go Right"
+                return SHOULD_RIGHT
         else:
             if waypoint == 'right':
                 if input['left'] != 'forward':
-                    return "Should Go Right"
+                    return SHOULD_RIGHT
                 else:
-                    return "Should Yield"
+                    return SHOULD_YIELD
             else:
-                return "Should Yield"
+                return SHOULD_YIELD
+
     def get_action_from_random(self):
+        """
+        Make a random action
+        :return:
+        """
         action_index = random.randrange(len(self.env.valid_actions))
         return action_index, self.env.valid_actions[action_index]
 
-    #Pick the action with the best Q
-    def get_action_from_policy(self,state):
-
-        max_q = MIN_INT
-        max_i = -1
-
-        actions = []
-        for i,q in enumerate(self.Q[state]):
-            if q > max_q:
-                actions.append((i,q))
-                max_i = i
-                max_q = q
+    def get_action_from_policy(self, state):
+        """
+        Pick the action with the best Q score
+        :param state:
+        :return:
+        """
+        max_i, max_q = max(enumerate(self.Q[state]), key=lambda x: x[1])
 
         if max_i > -1:
-            return (max_i, self.env.valid_actions[max_i])
+            return max_i, self.env.valid_actions[max_i]
         else:
             return self.get_action_from_random()
 
-    #Picks an action based on either a random action or policy,
-    #depending on an epsilon
     def get_action(self,state):
+        """
+        Picks an action based on either a random action or policy, depending on an epsilon
+        :param state:
+        :return:
+        """
         random_number = random.randrange(100)
         if self.epsilon > random_number :
             return self.get_action_from_random()
         else:
             return self.get_action_from_policy(state)
 
-    
     def reset(self, destination=None):
         self.planner.route_to(destination)
-        
-        # TODO: Prepare for a new trip; reset any variables here, if required
+
         # Reduce the epsilon value to reduce randomness over time
         if self.decrement_epsilon:
             self.epsilon = max(5, self.epsilon - 2)
 
-        #RESET STATS FOR RUN
+        #  RESET STATS FOR RUN
         self.rewards.append(self.reward_for_run)
         self.deadlines.append(self.deadline_for_run)
         self.reward_for_run = 0
         self.deadline_for_run = 0
-    
-
 
     def update(self, t):
-        # Gather inputs
+        """
+        Makes an action and updates Q-Score
+        :param t:
+        :return:
+        """
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
-        # TODO: Update state
         self.state = self.state_from_input(inputs, self.next_waypoint)
         
-        
         if self.state not in self.Q:
-            self.Q[self.state] = [0,0,0,0]
-        
-        # TODO: Select action according to your policy
+            self.Q[self.state] = [0, 0, 0, 0]
+
         action_index , action = self.get_action(self.state)
         
         # Execute action and get reward
@@ -122,18 +129,13 @@ class LearningAgent(Agent):
 
         q = reward + self.gamma * max(self.Q[self.state])
         self.Q[self.state][action_index] = (1 - self.alpha) * self.Q[self.state][action_index] + self.alpha * q
-        
-        #UPDATE STATS
+
         self.reward_for_run += reward
         self.deadline_for_run = deadline
 
-#print self.Q
-
-
-#print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 def test():
-    results = open("Assignment 4 Results",'w')
+    results = open("Results",'w')
     results.write("STATS\n")
     
     """Run the agent for a finite number of trials."""
@@ -154,11 +156,6 @@ def test():
         sim = Simulator(e, update_delay=0.0)  # reduce update_delay to speed up simulation
         sim.run(n_trials=75)  # press Esc or close pygame window to quit
 
-
-
-#        print "Rewards: ", a.rewards
-#        frame = pd.DataFrame(data = a.rewards[len(a.rewards)/2:])
-#        print frame.describe()
         results.write("Parameters: \n")
         results.write("Alpha: ")
         results.write(str(a.alpha))
@@ -174,6 +171,7 @@ def test():
         results.write(str(a.Q))
         results.write("\n\n\n")
     results.close()
+
 
 def run():
     #test()
